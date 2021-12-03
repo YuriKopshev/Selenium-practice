b0f0a0fcffc9d09d99cf8f172a981a8b0bbf762a
“Yuri <yura.kopshev@gmail.com>
2021-12-03 19:19:39 +0400

    added LiteCart.java

---
+++ src/test/java/LiteCart.java
@@ -0,0 +1,32 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class LiteCart {
+    private WebDriver driver;
+
+    @BeforeEach
+    public void start() {
+        driver = new FirefoxDriver();
+    }
+
+    @Test
+    public void loginTest() {
+        driver.get("http://localhost/litecart/admin/");
+        driver.findElement(By.name("username")).click();
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).click();
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).click();
+    }
+
+    @AfterEach
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+}
